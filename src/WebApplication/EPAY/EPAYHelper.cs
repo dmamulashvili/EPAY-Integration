8b1f73fc1bdabee19e3b42026ef47ddc70004f42
dmamulashvili <dmamulashvili@outlook.com>
2019-08-09 18:45:43 +0400

    Added Project & EPAY Partial Implementation

---
+++ src/WebApplication/EPAY/EPAYHelper.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WebApplication.EPAY
+{
+    public static class EPAYHelper
+    {
+        public static bool IsValidEPAYRequest(this HttpRequest httpRequest, EPAYRequest epayRequest, EPAYConfiguration config, out string responseContent)
+        {
+            responseContent = string.Empty;
+
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var queryValues = httpRequest.Query.Where(q => q.Key != "HASH_CODE").Select(s => s.Value.ToString()).ToList();
+
+                string hashOfInput = GetMd5Hash(md5Hash, string.Join(string.Empty, queryValues) + config.SecretKey);
+
+                var result = VerifyMd5Hash(md5Hash, hashOfInput, epayRequest.HashCode);
+
+                if (!result)
+                {
+                    responseContent = BuildREsponse(ResponseStatusCode.InvalidHashCode).ToString();
+                }
+
+                return result;
+            }
+        }
+
+        public static XElement BuildREsponse(ResponseStatusCode statusCode, int? customerBalance = null, string customerFirstName = null, string custmerLastName = null, int? paymentId = null)
+        {
+            if (statusCode == ResponseStatusCode.OK)
+            {
+                var receiptIdXElement = paymentId.HasValue ? new XElement("receipt-id", paymentId.Value) : null;
+                var debtXElement = customerBalance.HasValue ? new XElement("debt", customerBalance.Value) : null;
+
+                var additionalInfoXElement = !string.IsNullOrWhiteSpace(customerFirstName) && !string.IsNullOrWhiteSpace(custmerLastName) ?
+                    new XElement("additional-info",
+                        new XElement("parameter", new XAttribute("name", "first_name"), customerFirstName),
+                        new XElement("parameter", new XAttribute("name", "last_name"), custmerLastName)
+                    ) : null;
+
+                var result =
+                    new XElement("pay-response",
+                        new XElement("status",
+                            new XAttribute("code", (int)statusCode),
+                            statusCode.ToString()),
+                        new XElement("timestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds()),
+                        receiptIdXElement,
+                        debtXElement,
+                        additionalInfoXElement
+                    );
+
+                return result;
+            }
+            else
+            {
+                var result =
+                    new XElement("pay-response",
+                        new XElement("status",
+                            new XAttribute("code", (int)statusCode),
+                            new XAttribute("retry", false),
+                            statusCode.ToString()),
+                        new XElement("timestamp", DateTimeOffset.UtcNow.ToUnixTimeSeconds())
+                    );
+
+                return result;
+            }
+        }
+
+        static bool VerifyMd5Hash(MD5 md5Hash, string hashOfInput, string hash)
+        {
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return 0 == comparer.Compare(hashOfInput, hash);
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                stringBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ src/WebApplication/Pages/EPAY/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using WebApplication.EPAY;
+
+namespace WebApplication.Pages.EPAY
+{
+    [BindProperties(SupportsGet = true)]
+    public class IndexModel : PageModel
+    {
+        private readonly EPAYConfiguration _config;
+
+        public IndexModel(IOptions<EPAYConfiguration> options)
+        {
+            _config = options.Value;
+        }
+
+        public EPAYRequest EPAYRequest { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if(!HttpContext.Request.IsValidEPAYRequest(EPAYRequest, _config, out string responseContent))
+            {
+                return Content(responseContent);
+            }
+
+            return Content(string.Empty);
+        }
+    }
+}
